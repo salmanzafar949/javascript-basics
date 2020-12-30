a71a6f1f3662d2003a95c84d817cdab8204004a6
Salman Zafar <salman.zafar@innowi.com>
2020-12-30 15:19:18 +0500

    Palindrome word and sum of array

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 alert('hello')
+
+const array = [1,5,7,3]
+
+const sum = array.reduce((a, b) => a + b)
+
+console.log(`sum is ${sum}`)
+
+const isPalindrome = (word) => {
+    const wordLength = word.length - 1;
+    var reverseWord = "";
+
+    for (let i=wordLength; i>=0; i--)
+    {
+        reverseWord += word[i]
+
+        console.log(reverseWord)
+    }
+
+    return word === reverseWord;
+
+}
+
+console.log(`isPalindrome ${isPalindrome('cat')}`)
